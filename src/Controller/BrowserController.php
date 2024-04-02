3b3827a6e0f606d9451ad853eb874f78edf65f1c
Albert Fernandez <afernandez@designcodesolutions.com>
2024-04-02 10:54:04 -0400

    get url-encoded browsing destination

--- src/Controller/BrowserController.php
+++ src/Controller/BrowserController.php
@@ -3,16 +3,20 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class BrowserController extends AbstractController
 {
     #[Route('/browser', name: 'app_browser')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        // Get the url-encoded url that we want to browse
+        $url = urldecode( $request->query->get('p') );
+
         return $this->render('browser.html.twig',[
-            'url_content' => 'it works'
+            'url_content' => 'you want to see the content of '.$url
         ]);
     }
 }
