4371f4903e922cc7dbc97d7450abdff3789ca342
Albert Fernandez <afernandez@designcodesolutions.com>
2024-04-02 10:37:49 -0400

    twig bundle installed

--- src/Controller/BrowserController.php
+++ src/Controller/BrowserController.php
@@ -3,17 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class BrowserController extends AbstractController
 {
     #[Route('/browser', name: 'app_browser')]
-    public function index(): JsonResponse
+    public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/BrowserController.php',
-        ]);
+        return $this->render('browser.html.twig');
     }
 }
